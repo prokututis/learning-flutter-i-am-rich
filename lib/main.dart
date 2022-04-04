58e8397625e6d41867d47c88e7abdfe64fb55716
proku <andrejames1021@gmail.com>
2022-04-04 11:55:32 +0400

    understanding the basics

--- lib/main.dart
+++ lib/main.dart
@@ -6,23 +6,22 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-          backgroundColor: Colors.black,
-          appBar: AppBar(
-            title: const Text(
-              'I Am Rich',
-              style: TextStyle(color: Colors.black),
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: const Text(
+            'I Am Rich',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.orange[700],
+        ), // The bullshit at the top of the application
+        body: const Center(
+          child: Image(
+            image: AssetImage(
+              'C:/Users/Hizon/Desktop/Coding/flutter-dart-app-development/i_am_rich/i_am_rich/directory/diamond.png',
             ),
-            backgroundColor: Colors.orange[700],
-          ), // The bullshit at the top of the application
-          body: const Center(
-            child: Image(
-              image: NetworkImage(
-                'https://images.pexels.com/photos/7096/people-woman-coffee-meeting.jpg?cs=srgb&dl=pexels-startup-stock-photos-7096.jpg&fm=jpg',
-              ),
-            ),
-          )),
+          ),
+        ),
+      ),
     ),
   );
 }
-
-//026
